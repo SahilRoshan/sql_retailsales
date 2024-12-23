01701081535f7333ac731314fd1de5ade197edd8
SahilRoshan <sahiltyson12@gmail.com>
2024-12-23 11:41:19 +0530

    Update retail sale.sql

--- retail sale.sql
+++ retail sale.sql
@@ -17,6 +17,18 @@ cogs float null,
 total_sale float null
 );
 
+SELECT * FROM retail_sales
+WHERE 
+    sale_date IS NULL OR sale_time IS NULL OR customer_id IS NULL OR 
+    gender IS NULL OR age IS NULL OR category IS NULL OR 
+    quantity IS NULL OR price_per_unit IS NULL OR cogs IS NULL;
+
+DELETE FROM retail_sales
+WHERE 
+    sale_date IS NULL OR sale_time IS NULL OR customer_id IS NULL OR 
+    gender IS NULL OR age IS NULL OR category IS NULL OR 
+    quantity IS NULL OR price_per_unit IS NULL OR cogs IS NULL;
+
 -- selecting specific customer detailss
 select*from retail_sales
 where transactions_id=1679;
